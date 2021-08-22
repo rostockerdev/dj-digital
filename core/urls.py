40de4eb038d1f44493c3c1e18f54c05c4c8a7ef3
rostockerdev <rostockerdev@gmail.com>
2021-08-22 23:29:53 +0600

    :seedling: Add: Added robots.txt file

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Robots.txt
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
     path("admin/", admin.site.urls),
 ]
 
